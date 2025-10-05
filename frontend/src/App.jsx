1da0626f0094bac2b62943477d65492d985ef811
josdic1 <joshuadicker@gmail.com>
2025-10-05 13:18:07 -0400

    Initial setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import { healthCheck, getTracks } from './services/api'
-import './App.css'
 
 function App() {
   const [health, setHealth] = useState(null)
